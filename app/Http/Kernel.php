66f5007dc8661d1c78695f4f87d6c7e157373fd8
daniel <da.co>
2025-05-22 19:45:31 +0700

    nest

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,9 +36,9 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \App\Http\Middleware\AutoLogoutMiddleware::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \App\Http\Middleware\AutoLogoutMiddleware::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
